787738ce1ca9ad4c285718abc7636b3b68da62eb
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-11 00:27:26 -0600

    Add project files.

---
+++ Homework_Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Create an array of 3 names. Ask user which number to select.
+// When user gives you a number, return that name.
+// Check for invalid numbers.
+
+string[] names = new string[] { "Bob", "Jack", "Sparrow" };
+int num;
+do
+{
+	Console.Write($"Enter a number between 1 and {names.Length}: ");
+	int.TryParse(Console.ReadLine(), out num);
+
+	if (num > 0 && num <= names.Length)
+	{
+		Console.WriteLine(names[num - 1]);
+	}
+	else
+	{
+		Console.WriteLine("Invalid number.");
+	} 
+} while (!(num > 0 && num <= names.Length));
